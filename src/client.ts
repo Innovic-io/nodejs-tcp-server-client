50128cc7b6d87bee4639ea30d30bfa844635f0c6
ivana <ivanafustar@hotmail.com>
2018-05-28 16:17:47 +0200

    installed tslint and formatted code

--- src/client.ts
+++ src/client.ts
@@ -1,12 +1,12 @@
-import { createConnection } from 'net';
+import { createConnection } from "net";
 
 const client = createConnection({ port: 8125 }, () => {
-    console.log('connected to server');
+    console.log("connected to server");
 
-    client.on('data', data => {
+    client.on("data", (data) => {
         console.log(data.toString());
     });
 
-    client.write('London');
-    client.write('Paris');
+    client.write("London");
+    client.write("Paris");
 });
--- src/server.ts
+++ src/server.ts
@@ -1,45 +1,44 @@
-import { createServer } from 'net';
-import * as requestPromise from 'request-promise';
+import { createServer } from "net";
+import * as requestPromise from "request-promise";
 
-let connected = 0, port = 0;
+let connected = 0;
 const sockets = [];
 
-const server = createServer(socket => {
-    sockets.push({ 'socket': socket, 'port': socket.remotePort });
+const server = createServer((socket) => {
+    sockets.push({ socket, port: socket.remotePort });
 
     console.log(`Client connected! (${++connected})`);
-    socket.write(`Welcome, user!\nRemote address: ${socket.remoteAddress} Port: ${socket.remotePort}\nEnter the name of the city:\n`);
-    //socket.pipe(socket);
+    socket.write(`Welcome, user!\nRemote address: ${socket.remoteAddress}
+    Port: ${socket.remotePort}\nEnter the name of the city:\n`);
+    // socket.pipe(socket);
 
-    socket.on('end', () => {
+    socket.on("end", () => {
         console.log(`Client disconnected. (${--connected})`);
         sockets.splice(sockets.indexOf(socket), 1);
     });
 
-    socket.on('data', onData);
+    socket.on("data", onData);
 
     async function onData(data) {
 
-        const socketData = `Weather around the area ${data}${await getWeather(data.toString())}\nEnter the name of the city:\n`;
+        const socketData = `Weather around the area ${data}${await getWeather(data.toString())}
+        \nEnter the name of the city:\n`;
         broadcastMessage(socketData);
     }
 
     function broadcastMessage(socketData) {
-        sockets.forEach(socket => {
-            // if(socket.socket.)
-            socket.socket.write(socketData)
+        sockets.forEach((element) => {
+            element.socket.write(socketData);
         });
     }
 });
 
-
-server.on('error', err => {
-    throw err
+server.on("error", (err) => {
+    throw err;
 });
-server.listen({ port: 8125, host: '0.0.0.0' }, () => {
-    console.log('Server bound.')
+server.listen({ port: 8125, host: "0.0.0.0" }, () => {
+    console.log("Server bound.");
 });
-
 
 async function getWeather(data) {
 
@@ -47,24 +46,24 @@ async function getWeather(data) {
     try {
 
         const city = await requestPromise({
-            method: 'GET',
-            uri: `https://www.metaweather.com/api/location/search/?query=${data}`,
             json: true,
+            method: "GET",
+            uri: `https://www.metaweather.com/api/location/search/?query=${data}`,
+
         });
-        const latlong = city.map(value => value["latt_long"]);
+        const latlong = city.map((value) => value.latt_long);
 
         const weather = await requestPromise({
-            method: 'GET',
-            uri: `https://api.darksky.net/forecast/47c436a7e96c14385cb23a90b2435f05/${latlong}?units=si&lang=en`,
             json: true,
+            method: "GET",
+            uri: `https://api.darksky.net/forecast/47c436a7e96c14385cb23a90b2435f05/${latlong}?units=si&lang=en`,
         });
-        sentence = `Currently: ${weather["currently"]["summary"]}\nTemperature: ${weather["currently"]["temperature"].toFixed(0)}`;
+        sentence = `Currently: ${weather.currently.summary}\nTemperature: ${weather.currently.temperature.toFixed(0)}`;
 
+    } catch (e) {
+      sentence = `Cannot find the entered city.\n`;
     }
-    catch (e) {
-        sentence = `Cannot find the entered city.\n`;
-    }
-    finally {
-        return sentence;
-    }
+
+    return sentence;
+
 }
